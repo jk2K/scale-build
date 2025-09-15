5d56fc147c59409f216138d28f5668be77d319f9
jk2k <4025839+jk2k@users.noreply.github.com>
2025-09-15 21:56:35 +0800

    fix: use customed webui

--- scale_build/utils/manifest.py
+++ scale_build/utils/manifest.py
@@ -279,7 +279,7 @@ def validate_manifest():
             repo_path = url.path
 
         if hostname not in ['github.com', 'www.github.com'] or not repo_path.lower().strip('/').startswith((
-            'truenas/', 'ixsystems/'
+            'truenas/', 'ixsystems/', 'harbornas/'
         )):
             invalid_packages.append(package['name'])
 
